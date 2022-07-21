3d3bb392861df0175cf6e263bb8602d2f81b4e0a
EJBEAN <64283902+itsAnanth@users.noreply.github.com>
2022-07-21 19:35:43 +0530

    Update roles.js

--- commands/mod/roles.js
+++ commands/mod/roles.js
@@ -14,17 +14,17 @@ module.exports.run = (client, message) => {
             "description": "Check your level by using the `!rank` command in <#534605414139166722>\nEvery minute that you're chatting, you can gain between 15 and 25 XP",
             "color": 16763904,
             "fields": [{
-                    "name": "<:Blank:860122601849487410>",
+                    "name": "\u200b",
                     "value": "`Media & Links   `\n`Sticker access  `\n`Streaming perms `\n`Special channels`\n`Reaction perms  `",
                     "inline": true
                 },
                 {
-                    "name": "<:Blank:860122601849487410>",
+                    "name": "\u200b",
                     "value": "**10:** <@&719686530318467093>\n**20:** <@&675168719827238941>\n**30:** <@&674746305624408064>\n**40:** <@&675169426378850314>\n**50:** <@&855046303816744970>",
                     "inline": true
                 },
                 {
-                    "name": "<:Blank:860122601849487410>",
+                    "name": "\u200b",
                     "value": "**60:** <@&675536682602463274>\n**70:** <@&848418015816581150>\n**80:** <@&855047548795748352>\n**90:** <@&855047816712028190>\n**100:** <@&855048057754091530>",
                     "inline": true
                 }
