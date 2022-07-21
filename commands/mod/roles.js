ccb6b42fd69ded228eaa97a7895721ae5379710a
EJBEAN <64283902+itsAnanth@users.noreply.github.com>
2022-07-21 19:29:48 +0530

    Update roles.js

--- commands/mod/roles.js
+++ commands/mod/roles.js
@@ -36,28 +36,7 @@ module.exports.run = (client, message) => {
         "attachments": []
     }
 
-    message.channel.send({
-        content: mention != null ? `<@${mention}>,` : undefined,
-        embeds: [
-            new MessageEmbed()
-            .setTitle('MEE6 Roles:')
-            .setColor('BLURPLE')
-            .setAuthor('MEE6', 'https://cdn.discordapp.com/avatars/159985870458322944/b50adff099924dd5e6b72d13f77eb9d7.png')
-            .setDescription(`Check your level by using the \`!rank\` command in <#${id.channels['bot-comamnds']}>`)
-            .addField('How it works:', 'Every minute that you\'re messaging, you randomly gain between 15 and 25 XP. \n\nTo avoid spamming, earning XP is limited to once a minute per user.')
-            .addField('Level 10:', `<@&${id.roles.rookie}> \n\`\`\`diff\n+ Media and link perms.\n\`\`\``, true)
-            .addField('Level 20:', `<@&${id.roles.novice}> \n\`\`\`diff\n+ Sticker perms.\n\`\`\``, true)
-            .addField('Level 30:', `<@&${id.roles.active}> \n\`\`\`diff\n+ Streaming and video perms.\n\`\`\``, true)
-            .addField('Level 40:', `<@&${id.roles.apprentice}> \n\`\`\`diff\n+ Special channel and VC.\n\`\`\``, true)
-            .addField('Level 50:', `<@&${id.roles.devoted}> \n\`\`\`diff\n+ Reaction perms.\n\`\`\``, true)
-            .addField('Level 60:', `<@&${id.roles.legendary}>`, true)
-            .addField('Level 70:', `<@&${id.roles.mythical}>`, true)
-            .addField('Level 80:', `<@&${id.roles.nolife}>`, true)
-            .addField('Level 90:', `<@&${id.roles.godly}>`, true)
-            .addField('Level 100:', `<@&${id.roles.ascended}>`, true)
-            .setTimestamp()
-        ]
-    });
+    message.channel.send(embedObj);
 }
 
 module.exports.config = {
