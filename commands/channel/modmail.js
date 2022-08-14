51365d3c9618a0ad3d25ecc1712b7fb4d914a31f
EJBEAN <64283902+itsAnanth@users.noreply.github.com>
2022-08-14 23:48:53 +0530

    Update modmail.js

--- commands/channel/modmail.js
+++ commands/channel/modmail.js
@@ -125,7 +125,7 @@ module.exports.run = async(client, message) => {
 
     if (denyReasons == '') {
         const fetchData = await submissions_db.get(message.guild.id);
-        approvalRequest(client, message, embed.setTitle(`${embed.title} #${fetchData.subID}`), files);
+        approvalRequest(client, message, embed.setTitle(`${embed.title} #${fetchData.subID}`), files, embed.title.toLowerCase().includes('clip'));
         await submissions_db.set(message.guild.id, { subID: fetchData.subID + 1 });
         logger.messageDeleted(message, 'Modmail', 'NAVY');
     } else {
@@ -248,9 +248,9 @@ function autoDeny(message, denyReasons) {
     ).then(m => { delay(60000).then(() => m.delete()) });
 }
 
-async function approvalRequest(client, message, embed, files) {
+async function approvalRequest(client, message, embed, files, bypassLink) {
     if (embed.image) embed = await proxyEmbedImage(client, embed);
-    if (embed.description.includes('https://')) {
+    if (embed.description.includes('https://') && bypassLink) {
         const [tempEmbed, links] = AttachEmbedImages(embed)
         files.push(...links);
         embed = tempEmbed;
